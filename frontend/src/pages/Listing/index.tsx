b8ee23cab6fc4b9497195527a11806dee9afc013
katianne23 <katianne40@gmail.com>
2022-01-14 15:50:40 -0300

    First request

--- frontend/src/pages/Listing/index.tsx
+++ frontend/src/pages/Listing/index.tsx
@@ -1,7 +1,14 @@
 import MovieCard from "components/MovieCard";
 import Pagination from "components/Pagination";
+import axios from "axios";
+import { BASE_URL} from "utils/requests";
 
 function Listing() {
+  //forma errada
+ axios.get(`${BASE_URL}/movies?size=12&page=0`)
+      .then(response =>{
+        console.log(response.data);
+      })
   return (
     <>
       <Pagination />
